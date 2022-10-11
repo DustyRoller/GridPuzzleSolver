dda8e28b513e9be272211420e87175faa46167d9
DustyRoller <DustyRoller@github.com>
2022-10-11 22:18:53 +0100

    Created solution with empty projects

---
+++ GridPuzzleSolverUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GridPuzzleSolverUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
