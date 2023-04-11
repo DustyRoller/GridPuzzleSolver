66543adc8f781d41e8a51c0519d0968b8efcd15d
DustyRoller <DustyRoller@github.com>
2023-04-11 20:53:01 +0100

    Added a ParserFactory to generate the required parser based on the input file

---
+++ GridPuzzleSolver/Parser/ParserFactory.cs
@@ -0,0 +1,45 @@
+﻿using GridPuzzleSolver.KakuroSolver.Parser;
+using GridPuzzleSolver.SudokuSolver.Parser;
+
+namespace GridPuzzleSolver.Parser
+{
+    /// <summary>
+    /// The ParserFactory is responsible for loading the correct Parser based
+    /// on the file that has been provided.
+    /// </summary>
+    internal static class ParserFactory
+    {
+        public static IParser GetParser(string puzzleFile)
+        {
+            if (string.IsNullOrEmpty(puzzleFile))
+            {
+                throw new ArgumentException("Puzzle file is null or empty.", nameof(puzzleFile));
+            }
+            // Get the file extension from the file name.
+            var fileExtension = Path.GetExtension(puzzleFile);
+
+            if (string.IsNullOrEmpty(fileExtension))
+            {
+                throw new ParserException(
+                    $"Failed to get file extension from puzzle file - {puzzleFile}.");
+            }
+
+            IParser parser;
+
+            if (fileExtension == KakuroParser.KakuroPuzzleFileExtension)
+            {
+                parser = new KakuroParser();
+            }
+            else if (fileExtension == SudokuParser.SudokuPuzzleFileExtension)
+            {
+                parser = new SudokuParser();
+            }
+            else
+            {
+                throw new ParserException($"File extension \'{fileExtension}\' not recognised.");
+            }
+
+            return parser;
+        }
+    }
+}
---
+++ GridPuzzleSolverUnitTests/Parser/ParserFactoryUnitTests.cs
@@ -0,0 +1,70 @@
+﻿using GridPuzzleSolver.KakuroSolver.Parser;
+using GridPuzzleSolver.SudokuSolver.Parser;
+using NUnit.Framework;
+
+namespace GridPuzzleSolver.Parser.UnitTests
+{
+    [TestFixture]
+    public class ParserFactoryUnitTests
+    {
+        [TestCase]
+        public void ParserFactory_GetParser_ThrowsExceptionWithNullFileName()
+        {
+            string inputFile = null;
+
+            var ex = Assert.Throws<ArgumentException>(() => ParserFactory.GetParser(inputFile));
+
+            Assert.AreEqual("Puzzle file is null or empty. (Parameter 'puzzleFile')", ex.Message);
+        }
+
+        [TestCase]
+        public void ParserFactory_GetParser_ThrowsExceptionWithEmptyFileName()
+        {
+            var inputFile = "";
+
+            var ex = Assert.Throws<ArgumentException>(() => ParserFactory.GetParser(inputFile));
+
+            Assert.AreEqual("Puzzle file is null or empty. (Parameter 'puzzleFile')", ex.Message);
+        }
+
+        [TestCase]
+        public void ParserFactory_GetParser_ThrowsExceptionWithInvalidFileName()
+        {
+            var inputFile = "adodgyfilename";
+
+            var ex = Assert.Throws<ParserException>(() => ParserFactory.GetParser(inputFile));
+
+            Assert.AreEqual($"Failed to get file extension from puzzle file - {inputFile}.", ex.Message);
+        }
+
+        [TestCase]
+        public void ParserFactory_GetParser_ThrowsExceptionWithUnknownFileExtension()
+        {
+            var inputFile = "puzzlefile.txt";
+
+            var ex = Assert.Throws<ParserException>(() => ParserFactory.GetParser(inputFile));
+
+            Assert.AreEqual($"File extension \'{Path.GetExtension(inputFile)}\' not recognised.", ex.Message);
+        }
+
+        [TestCase]
+        public void ParserFactory_GetParser_ReturnsKakuroParser()
+        {
+            var inputFile = "puzzlefile.kak";
+
+            var parser = ParserFactory.GetParser(inputFile);
+
+            Assert.IsInstanceOf(typeof(KakuroParser), parser);
+        }
+
+        [TestCase]
+        public void ParserFactory_GetParser_ReturnsSudokuParser()
+        {
+            var inputFile = "puzzlefile.sud";
+
+            var parser = ParserFactory.GetParser(inputFile);
+
+            Assert.IsInstanceOf(typeof(SudokuParser), parser);
+        }
+    }
+}
