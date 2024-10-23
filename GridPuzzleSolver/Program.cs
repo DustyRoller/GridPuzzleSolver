c4a6495289b479d3e4455b9c80ad26490dc48c38
DustyRoller <DustyRoller@github.com>
2024-10-23 09:33:17 +0100

    attempt at xml serialization

--- GridPuzzleSolver/Program.cs
+++ GridPuzzleSolver/Program.cs
@@ -1,6 +1,8 @@
-﻿using GridPuzzleSolver.Parser;
+﻿using GridPuzzleSolver.Components;
+using GridPuzzleSolver.Parser;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
 
 [assembly: InternalsVisibleTo("GridPuzzleSolverUnitTests")]
 
@@ -12,57 +14,20 @@ namespace GridPuzzleSolver
     internal static class Program
     {
         /// <summary>
-        /// Runs the grid solver program. Parses the given puzzle file and attempts to solve it.
+        /// a.
         /// </summary>
-        /// <param name="puzzleFilePath">The path to the puzzle file containing the puzzle to be solved.</param>
-        /// <exception cref="ArgumentException">Thrown if the puzzle file path is invalid.</exception>
-        /// <exception cref="ParserException">Thrown if no suitable parser for the puzzle is found.</exception>
-        public static void Run(string puzzleFilePath)
+        /// <typeparam name="T">b.</typeparam>
+        /// <param name="filepath">c.</param>
+        /// <returns>d.</returns>
+        public static T? DeserializeToObject<T>(string filepath)
+            where T : class
         {
-            // Validate the input.
-            if (string.IsNullOrEmpty(puzzleFilePath))
+            var xmlSerializer = new XmlSerializer(typeof(T));
+
+            using (var streamReader = new StreamReader(filepath))
             {
-                throw new ArgumentException("Puzzle file path is null or empty.", nameof(puzzleFilePath));
+                return xmlSerializer.Deserialize(streamReader) as T;
             }
-
-            // Get the file extension from the file name.
-            var puzzleFileExtension = Path.GetExtension(puzzleFilePath);
-
-            if (string.IsNullOrEmpty(puzzleFileExtension))
-            {
-                throw new ArgumentException(
-                    $"Failed to get file extension from puzzle file - {puzzleFilePath}.");
-            }
-
-            // Get the parser for the given puzzle type.
-            var parser = ParserFactory.GetParser(puzzleFileExtension);
-
-            var puzzle = parser.ParsePuzzle(puzzleFilePath);
-
-            // Time how long it takes to solve the puzzle.
-            var stopwatch = Stopwatch.StartNew();
-
-            if (puzzle.Solve())
-            {
-                Console.WriteLine("Successfully solved puzzle");
-            }
-            else
-            {
-                Console.WriteLine("Failed to solve puzzle\n");
-                Console.WriteLine($"{puzzle.NumberOfUnsolvedCells} cells remain unsolved");
-            }
-
-            // Stop the stopwatch.
-            stopwatch.Stop();
-
-            // Print out the puzzle.
-            Console.WriteLine();
-            Console.WriteLine(puzzle.ToString());
-
-            // Print out how long it took to solve the puzzle.
-            Console.WriteLine();
-            var timeTaken = (stopwatch.ElapsedMilliseconds / 1000.0).ToString("F2");
-            Console.WriteLine($"Time taken: {timeTaken}s");
         }
 
         private static void Main(string[] args)
@@ -73,7 +38,14 @@ namespace GridPuzzleSolver
                 Environment.Exit(1);
             }
 
-            Run(args[0]);
+            var puzzle = DeserializeToObject<Puzzle>(args[0]);
+            if (puzzle == null)
+            {
+                throw new ArgumentException("Failed to deserialize");
+            }
+
+            Console.WriteLine($"Height: {puzzle.Height}");
+            Console.WriteLine($"Width: {puzzle.Width}");
         }
     }
 }
---
+++ GridPuzzleSolver/Puzzles/Suguru/Parser/SuguruParser.cs
@@ -0,0 +1,21 @@
+﻿using GridPuzzleSolver.Components;
+using GridPuzzleSolver.Parser;
+
+namespace GridPuzzleSolver.Puzzles.Suguru.Parser
+{
+    /// <summary>
+    /// Class to parse Suguru puzzles from text files.
+    /// </summary>
+    internal class SuguruParser : BaseParser
+    {
+        /// <summary>
+        /// Parse the given file to generate a Puzzle, ready to be solved.
+        /// </summary>
+        /// <param name="puzzleFilePath">The path to the file containing the puzzle.</param>
+        /// <returns>A Puzzle object.</returns>
+        public override Puzzle ParsePuzzle(string puzzleFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
