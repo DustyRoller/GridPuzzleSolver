be5f55c34a35d96565bf45602e8b6777551d3735
DustyRoller <DustyRoller@github.com>
2025-07-07 21:39:55 +0100

    fixed test puzzle

--- GridPuzzleSolver/Puzzles/KillerSudoku/KillerSudokuPuzzle.cs
+++ GridPuzzleSolver/Puzzles/KillerSudoku/KillerSudokuPuzzle.cs
@@ -87,7 +87,7 @@ namespace GridPuzzleSolver.Puzzles.KillerSudoku
         {
             if (cells.Count != 9)
             {
-                throw new GridPuzzleSolverException($"Section can only contain 9 cells but received: {Cells.Count}");
+                throw new GridPuzzleSolverException($"Section can only contain 9 cells but received: {cells.Count}");
             }
 
             var section = new SudokuSection();
